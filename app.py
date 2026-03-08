8700354a44224804fc870f005826693e11b9dcf5
Cornolly <nick@summitfx.com>
2026-03-08 11:05:41 +0000

    Update app.py

--- app.py
+++ app.py
@@ -298,7 +298,7 @@ def handle_pipedrive_webhook():
                 # ✅ Variable handling logic per template
                 if template_name == "24hrs":
                     variables = {}
-                elif template_name in ["payment_account", "payment_which", "quote_amount", "feefo_request", "request_settlement_confirmation", "quote_tips", "scio_terms", "scio_and_equals_terms", "1k_reminder", "jumio_ebury"]:
+                elif template_name in ["payment_account", "payment_which", "quote_amount", "feefo_request", "request_settlement_confirmation", "quote_tips", "scio_terms", "scio_and_equals_terms", "1k_reminder", "jumio_ebury", "intro_thanks"]:
                     # Split into two variables by the first space
                     parts = field_value.strip().split(" ", 1)
                     variables = {
